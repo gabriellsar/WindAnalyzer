fe955fd3f6f30e0fea9525ac2deff62d15740d73
Gabriel Rosas <gabrielsrosas@gmail.com>
2025-10-03 10:37:50 -0300

    update config

--- config_token.R
+++ config_token.R
@@ -1,4 +1,4 @@
 access_token <- ""
-refresh_token <- "[REFRESH_TOKEN]"
-client_id <- "[CLIENT_ID]"
-client_secret <- "[SECRET_KEYT"
+refresh_token <- "FX5PY5_4VX4AAAAAAAAAATPKWtaEsZ2etakTENQmuXu3zuxJ7YpJeoG-W2YybNu-"
+client_id <- "56t7cm9qsb6srpu"
+client_secret <- "8vlwcrge1831jvm"
